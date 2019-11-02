d97bbaf01ff55490461915920c9bef6496e9a08c
jakesIII <jayruma@yahoo.com>
2019-11-03 02:11:55 +0300

    generate new component with a form for user input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
